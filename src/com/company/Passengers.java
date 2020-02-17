9ac25d203f82005d9106fda6232f576d4f024b58
admin <rsk.mva@gmail.com>
2020-02-17 23:28:51 +0600

    sleep

--- src/com/company/Passengers.java
+++ src/com/company/Passengers.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import javax.swing.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 public class Passengers  extends Thread{
     Semaphore ps;
     int id;
-    private CountDownLatch cdl =new CountDownLatch(100);
+    private CountDownLatch cdl;
 
     public Passengers(Semaphore ps, int id, CountDownLatch cdl) {
         this.ps = ps;
@@ -24,14 +25,14 @@ public class Passengers  extends Thread{
             try {
                 ps.acquire();
                 System.out.println("Passenger " + id+ " comes to the cash counter");
-                sleep(3000);
+                sleep(1000);
 
                 System.out.println("Passenger " + id+ " pays for the ticket");
-                sleep(3000);
+                sleep(1000);
 
                 System.out.println("Passenger " + id+ " leaving the cash counter and goes to the bus ");
                 ps.release();
-                sleep(5000);
+                sleep(3000);
                 cdl.countDown();
             } catch (InterruptedException e) {
                 System.out.println(" interrupted");
